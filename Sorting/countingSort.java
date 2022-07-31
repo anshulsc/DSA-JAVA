3fd0ab9644854ff3885e8e70d144b5a6114a8082
Anshul Singh <anshulsinghchambial@gmail.com>
2022-07-31 12:37:48 +0530

    Counting Sort

--- Sorting/countingSort.java
+++ Sorting/countingSort.java
@@ -1,5 +1,40 @@
 package com.company.Sorting;
 
+import java.sql.ClientInfoStatus;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class countingSort {
 
+    public static void main(String[] args) {
+
+        int arr[] = {3,3,6,7,5,5,3,2,3,5,9,8,8};
+
+        countSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static int[] countSort(int[] arr) {
+
+        ArrayList<ArrayList> list = new ArrayList<>();
+
+        for(int i = 0 ; i<=9 ; i++){
+            list.add(new ArrayList<>());
+        }
+
+        for(int i = 0 ; i < arr.length ; i++){
+            list.get(arr[i]).add(arr[i]);
+        }
+
+        ArrayList<Integer> sol = new ArrayList<>();
+
+        for(int i = 0 ; i<list.size() ; i++){
+            sol.addAll(list.get(i));
+        }
+        for(int i = 0 ; i< arr.length; i++){
+            arr[i] = sol.get(i);
+        }
+
+        return arr;
+    }
 }
